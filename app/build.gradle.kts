26a94c10b3a1d49eab9a19bd4ccf85a312a0456f
ngkhacduy74 <duycaduoc0704@gmail.com>
2025-07-23 23:04:38 +0700

    build

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,6 +29,14 @@ android {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
+    
+    packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+            excludes += "META-INF/NOTICE.md"
+            excludes += "META-INF/LICENSE.md"
+        }
+    }
 }
 
 dependencies {
